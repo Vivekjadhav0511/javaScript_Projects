36032e7f8f306b0944acba889f7978811703982a
Vivek Jadhav <vivekjadhav0511@gmail.com>
2024-05-27 16:05:20 +0530

    Color Changer

--- 01_colorChanger/script.js
+++ 01_colorChanger/script.js
@@ -7,17 +7,19 @@ let body = document.querySelector("body")
 color.forEach((color)=>{
     color.addEventListener('click',(e)=>{
         console.log(e);
-        console.log(e.target);
+        console.log(e.target.id);
      if(e.target.id=='black') {
         body.style.backgroundColor="black"
         h1.style.color="white"
      }else if(e.target.id=='white'){
-        body.style.backgroundColor="grey"
+        body.style.backgroundColor=" #a99f9f"
         h1.style.color="black"
      }else if(e.target.id=='blue'){
         body.style.backgroundColor="blue"
-     }else if (e.target.id=='yellow')
-        body.style.backgroundColor="yellow"
+     }else if (e.target.id=='yellow'){
+      body.style.backgroundColor="yellow"
+     }
+       
     })
 })
 
