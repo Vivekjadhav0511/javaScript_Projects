be6631fca6b8bc62f3a0473de81b03cea3459da6
Vivek Jadhav <vivekjadhav0511@gmail.com>
2024-05-28 17:13:21 +0530

    project

---
+++ 04_GuessTheNumber/script.js
@@ -0,0 +1,10 @@
+
+let random = Math.floor(Math.random() * 10 +1)
+let submitBtn=document.querySelector('#submit')
+let guessFeild =document.querySelector("#guessFeild")
+let guesses = document.querySelector(".guesses")
+let lastResult =document.querySelector('.lastResult')
+let lowOrHi =document.querySelector('.lowOrHi')
+let resultParas=document.querySelector('.resultParas')
+ 
+
